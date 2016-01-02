f20e6c037ee010339727844b0c30585c94da7721
Tahir <tahirlanre@hotmail.com>
2016-01-02 11:52:41 +0100

    Removed unnecessary code

--- app/uploader/views.py
+++
@@ -1,9 +0,0 @@
-import requests
-from flask import request, render_template, flash, redirect, url_for, \
-    session, Blueprint, g
-
-auth = Blueprint('auth', __name__)
-
-@auth.route('/uploader')
-def uploader():
-    return render_template('uploader.html')
