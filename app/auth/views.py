34bdc12c238dc17a8eafb3a762bf51807ca2f0d6
Tahir <tahirlanre@hotmail.com>
2016-01-04 15:54:13 +0100

    Added custom HTTP error handlers

--- app/auth/views.py
+++ app/auth/views.py
@@ -108,6 +108,14 @@ def logout():
     return redirect(url_for('auth.home'))
     
 
+@auth.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+""""@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500"""
 
 
   
